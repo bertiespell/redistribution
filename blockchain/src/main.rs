a020884318a600a37cb8e4b7add70a5ae47d83e9
Bertie <berniesnell@gmail.com>
2019-07-02 18:37:27 -0400

    Add basic block and a hashing function - include openssl dependency and hex libraries

--- blockchain/src/main.rs
+++ blockchain/src/main.rs
@@ -1,3 +1,24 @@
+use openssl::sha;
+use hex;
 fn main() {
     println!("Hello, world!");
+}
+
+struct Block {
+    index: u32, // height of the blockchain
+    timestamp: String,
+    data: u32,
+    hash: String,
+    previous_hash: String
+}
+
+fn calculate_hash(index: &i32, previous_hash: &str, timestamp: &str, data: &str) -> String {
+    let mut hasher = sha::Sha256::new();
+    hasher.update(&index.to_be_bytes());
+    hasher.update(&previous_hash.as_bytes());
+    hasher.update(&timestamp.as_bytes());
+    hasher.update(&data.as_bytes());
+
+    let hash = hasher.finish();
+    hex::encode(hash)
 }
