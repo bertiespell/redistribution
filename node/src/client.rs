a53a69029f004c95a278831a6eff3ac06b8febb7
Bertie <berniesnell@gmail.com>
2019-07-12 17:38:42 -0400

    refactor client to use new protocol enum

--- node/src/client.rs
+++ node/src/client.rs
@@ -3,11 +3,18 @@ use std::net::{TcpStream};
 use blockchain::{Blockchain, Encodable};
 use std::sync::{Arc};
 use std::net::{SocketAddr};
+extern crate tokio;
+use crate::protocol_message::ProtocolMessage;
+
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
 #[derive(Debug)]
 pub struct Client {
+	id: Option<u128>,
     blockchain: Blockchain,
-    peers: Vec<String>,
+    peers: Vec<u128>, // list of IDs
 }
 
 impl Client {
@@ -16,40 +23,71 @@ impl Client {
         let peers = vec!();
 
         Arc::new(Client {
+			id: None,
             blockchain,
             peers,
         })
     }
 
-    pub fn discover_peers(&self, root: SocketAddr) {
-        let mut stream = TcpStream::connect(root).unwrap();
-        let get_blocks = b"getBlocks"; // send getblockchain
-        stream.write(get_blocks);
+	pub fn initialise(&self, root: SocketAddr) {
+		let mut stream = TcpStream::connect(root).unwrap();
+		let add_me = ProtocolMessage::AddMe.as_str();
+        stream.write(add_me.as_bytes());
         stream.read(&mut [0; 128]);
-        // TODO: Write discovery peers to own blockchain
+        
+        //TODO:
+		// send add me message - recieves ID
+		// ORIGINAL/ROOT node - adds ID to list and broadcasts result to everyone
+		// New peer with new ID asks for list of nodes...
+		// sends back peers
+	}
+
+    // TODO: All these tcp streams are repeated - they should be in a wrapper? Using Tokio maybe?
+    fn add_me(&self, root: SocketAddr) {
+        let mut stream = TcpStream::connect(root).unwrap();
+		let add_me = ProtocolMessage::AddMe.as_str();
+        stream.write(add_me.as_bytes());
+        stream.read(&mut [0; 128]);
+    }
+
+    fn get_peers(&self, root: SocketAddr) {
+        let mut stream = TcpStream::connect(root).unwrap();
+        let get_peers = ProtocolMessage::GetPeers.as_str();
+        stream.write(get_peers.as_bytes());
+        stream.read(&mut [0; 128]);
+        // TODO: Write peers to own hashtable
     }
 
     pub fn handle_incoming(&self, mut stream: TcpStream) {
         // TODO: this should parse different messages and route them appropriately
         let mut buffer = [0; 512];
         stream.read(&mut buffer).unwrap();
-        println!("Incoming");
-        let get_blocks = b"getBlocks"; // send getblockchain
-        let mint_block = b"mintBlock"; // generated next block and send it
-        let get_peers = b"peers"; // iterate over sockets
-        let add_peer = b"addPeer"; // connect to peers
 
-        if buffer.starts_with(get_blocks) {
-            let blocks = self.blockchain.encode(); // TODO: Let's handle this - blockchain might not be initialised here
+        if buffer.starts_with(ProtocolMessage::GetBlocks.as_str().as_bytes()) {
+            let blocks = self.blockchain.encode();
             println!("Received get request. Sending: {:?}", &blocks);
             stream.write(&blocks).unwrap();
             stream.flush().unwrap();
-        } else if buffer.starts_with(mint_block) {
+        } else if buffer.starts_with(ProtocolMessage::MintBlock.as_str().as_bytes()) {
             //
-        } else if buffer.starts_with(get_peers) {
-            //
-        } else if buffer.starts_with(add_peer) {
-            //
+        } else if buffer.starts_with(ProtocolMessage::GetPeers.as_str().as_bytes()) {
+            // check the node is known in hash table...
+            // send back list of peers
+        } else if buffer.starts_with(ProtocolMessage::AddMe.as_str().as_bytes()) {
+			// TODO: ensure we're using UUID. Here we just use an incrementing ID - ideally in the future one node won't store *all* other nodes in its peers... so we'll need a smarter system
+			let mut highest_id: u128 = 0;
+			let mut peers = self.peers.iter();
+
+            while let Some(peer_id) = peers.next() {
+                if highest_id < *peer_id {
+                    highest_id = *peer_id;
+                }
+            }
+
+			highest_id = highest_id + 1;
+            println!("Sending new client id: {}", highest_id);
+			stream.write(&highest_id.to_be_bytes()).unwrap();
+            stream.flush().unwrap();
         }
     }
 }
